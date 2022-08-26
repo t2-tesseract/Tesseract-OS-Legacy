cf44ee03ae6226e156cf65b24be443f691482105
Kokolor <kokolor.mail@gmail.com>
2022-08-26 17:12:39 +0200

    Iso script and uncomment shell

--- Source/Kernel/Kernel.c
+++ Source/Kernel/Kernel.c
@@ -13,80 +13,24 @@ void _start(){
 
     TerminalClear();
 
-	Printk("Char* Hello = %s\n\n", Hello);
-	Delay(540);
-	Printk("Char* Hello = %s\n\n", Hello);
-	Delay(540);
-	Printk("Char* Hello = %s\n\n", Hello);
-	Delay(540);
-	Printk("Char* Hello = %s\n\n", Hello);
-	Delay(540);
-	Printk("Char* Hello = %s\n\n", Hello);
-	Delay(540);
-	Printk("Char* Hello = %s\n\n", Hello);
-	Delay(540);
-	Printk("Char* Hello = %s\n\n", Hello);
-	Delay(540);
-	Printk("Char* Hello = %s\n\n", Hello);
-	Delay(540);
-	Printk("Char* Hello = %s\n\n", Hello);
-	Delay(540);
-	Printk("Char* Hello = %s\n\n", Hello);
-	Delay(540);
-	Printk("Char* Hello = %s\n\n", Hello);
-	Delay(540);
-	Printk("Char* Hello = %s\n\n", Hello);
-	Delay(540);
-	Printk("Char* Hello = %s\n\n", Hello);
-	Delay(540);
-	Printk("Char* Hello = %s\n\n", Hello);
-	Delay(540);
-	Printk("Char* Hello = %s\n\n", Hello);
-	Delay(540);
-	Printk("Char* Hello = %s\n\n", Hello);
-	Delay(540);
-	Printk("Char* Hello = %s\n\n", Hello);
-	Delay(540);
-	Printk("Char* Hello = %s\n\n", Hello);
-	Delay(540);
-	Printk("Char* Hello = %s\n\n", Hello);
-	Delay(540);
-	Printk("Char* Hello = %s\n\n", Hello);
-	Delay(540);
-	Printk("Char* Hello = %s\n\n", Hello);
-	Delay(540);
-	Printk("Char* Hello = %s\n\n", Hello);
-	Delay(540);
-	Printk("Char* Hello = %s\n\n", Hello);
-	Delay(540);
-	Printk("Char* Hello = %s\n\n", Hello);
-	Delay(540);
-	Printk("Char* Hello = %s\n\n", Hello);
-	Delay(540);
-	Printk("Char* Hello = %s\n\n", Hello);
-	Delay(540);
-	Printk("Char* Hello = %s\n\n", Hello);
-	Delay(540);
-	Printk("Char* Hello = %s\n\n", Hello);
-	Delay(540);
-	// DebugWrite("GDT Loaded.\n", 0, true);
-	// DebugWrite("IDT Loaded.\n\n", 0, true);
+	DebugWrite("GDT Loaded.\n", 0, true);
+	DebugWrite("IDT Loaded.\n\n", 0, true);
 
-	// DebugWrite("Test kernel msg\n", 0, false);
-	// DebugWrite("Test warning msg\n", 1, false);
-	// DebugWrite("Test error msg\n", 2, false);
-	// DebugWrite("Test log msg\n\n", 3, false);
+	DebugWrite("Test kernel msg\n", 0, false);
+	DebugWrite("Test warning msg\n", 1, false);
+	DebugWrite("Test error msg\n", 2, false);
+	DebugWrite("Test log msg\n\n", 3, false);
 
-	// TerminalShell();
+	TerminalShell();
 
-	// EnableInterrupts;
+	EnableInterrupts;
 
-	// while (1)
-	// {
-	// 	TerminalWrite("Hello\n");
-	// 	Delay(500);
-	// 	TerminalWrite("Binouka\n");
-	// }
+	while (1)
+	{
+		TerminalWrite("Hello\n");
+		Delay(500);
+		TerminalWrite("Binouka\n");
+	}
 	
 
     while(1);
